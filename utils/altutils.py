842ec04a62735083bc42f837c8337b72e1fbea7f
Lu Gan <ganlu@umich.edu>
2022-08-28 17:22:09 -0700

    dataloader for m3fd

--- utils/altutils.py
+++ utils/altutils.py
@@ -79,7 +79,7 @@ def get_mscoco(dataset_root, batch_size, train):
             transforms.ToTensor(),
             transforms.Normalize(mean=(0.4017, 0.3791, 0.3656), std=(0.2093, 0.2019, 0.1996))
         ])
-        mscoco_dataset = datasets.ImageFolder(root=os.path.join(dataset_root, 'uda_data/mscoco/train'),
+        mscoco_dataset = datasets.ImageFolder(root=os.path.join(dataset_root, 'uda_data/mscoco2/train'),
                                              transform=pre_process)
 
         weight = make_weight_for_balanced_classes(mscoco_dataset.imgs, len(mscoco_dataset.classes))
@@ -97,7 +97,7 @@ def get_mscoco(dataset_root, batch_size, train):
                                           transforms.Normalize(
                                               mean=(0.3961, 0.3743, 0.3603),
                                               std=(0.2086, 0.2012, 0.1987))])
-        mscoco_dataset = datasets.ImageFolder(root=os.path.join(dataset_root, 'uda_data/mscoco/val'),
+        mscoco_dataset = datasets.ImageFolder(root=os.path.join(dataset_root, 'uda_data/mscoco2/val'),
                                             transform=pre_process)
 
         mscoco_data_loader = torch.utils.data.DataLoader(
@@ -108,7 +108,53 @@ def get_mscoco(dataset_root, batch_size, train):
     return mscoco_data_loader
 
 
-def get_flir(dataset_root, batch_size, train):
+def get_m3fd(dataset_root, batch_size, train):
+    """Get M3FD datasets loader
+
+    Args:
+        dataset_root (str): path to the dataset folder
+        batch_size (int): batch size
+        train (bool): create loader for training or test set
+
+    Returns:
+        obj: dataloader object for M3FD dataset
+    """ 
+    # dataset and data loader
+    if train:
+        pre_process = transforms.Compose([transforms.Resize((224, 224)),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize(
+                                          mean=(0.4810, 0.4810, 0.4810),
+                                          std=(0.2081, 0.2081, 0.2081))])
+        m3fd_dataset = datasets.ImageFolder(root=os.path.join(dataset_root, 'uda_data/m3fd/train'),
+                                             transform=pre_process)
+        weight = make_weight_for_balanced_classes(m3fd_dataset.imgs, len(m3fd_dataset.classes))
+        weight=torch.DoubleTensor(weight)
+
+        sampler = torch.utils.data.sampler.WeightedRandomSampler(weight, len(weight))
+
+        m3fd_data_loader = torch.utils.data.DataLoader(
+            dataset=m3fd_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            sampler=sampler, num_workers=4, pin_memory=True, drop_last=True)
+    else:
+        pre_process = transforms.Compose([transforms.Resize((224, 224)),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize(
+                                          mean=(0.4810, 0.4810, 0.4810),
+                                          std=(0.2081, 0.2081, 0.2081))])
+        m3fd_dataset = datasets.ImageFolder(root=os.path.join(dataset_root, 'uda_data/m3fd/val'),
+                                            transform=pre_process)
+        m3fd_data_loader = torch.utils.data.DataLoader(
+            dataset=m3fd_dataset,
+            batch_size=batch_size,
+            shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
+
+    return m3fd_data_loader
+
+
+def get_flir(dataset_root, batch_size, train, pseudo_label=False):
     """Get FLIR datasets loader
 
     Args:
@@ -119,6 +165,24 @@ def get_flir(dataset_root, batch_size, train):
     Returns:
         obj: dataloader object for FLIR dataset
     """ 
+    # data loader for pseudo label
+    if pseudo_label:
+        pre_process = transforms.Compose([transforms.Resize((224, 224)),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize(
+                                          mean=(0.5776, 0.5776, 0.5776),
+                                          std=(0.1319, 0.1319, 0.1319))])
+        flir_dataset = datasets.ImageFolder(root=os.path.join(dataset_root, 'uda_data/flir/train'),
+                                             transform=pre_process)
+
+        flir_data_loader = torch.utils.data.DataLoader(
+            dataset=flir_dataset,
+            batch_size=1,
+            shuffle=False,
+            num_workers=1, pin_memory=True, drop_last=False)
+
+        return flir_data_loader, flir_dataset.samples
+
     # dataset and data loader
     if train:
         pre_process = transforms.Compose([transforms.Resize((224, 224)),
